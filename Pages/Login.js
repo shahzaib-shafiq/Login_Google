7875758fd20e34494a47c2385745f525feb311e5
Shahzaib Shafiq <73356944+shahzaibshafiq100@users.noreply.github.com>
2023-11-23 12:16:07 +0500

    Login Screen Created

---
+++ Pages/Login.js
@@ -0,0 +1,65 @@
+import { View, Text, Image, StyleSheet } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import Colors from '../Shared/Colors'
+import { TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+
+export default function Login() {
+    return (
+
+        <View>
+            <Image source={require('./../assets/login.png')}></Image>
+
+            <View style={styles.container}>
+                <Text style={styles.welcomeText}>Welcome to SSNA</Text>
+                <Text style={{
+                    textAlign: 'center',
+                    marginTop: 80, fontSize: 20
+                }}>Login/Signup</Text>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => promptAsync()}>
+                    <Ionicons name="logo-google" size={24}
+                        color="white" style={{ marginRight: 10 }} />
+                    <Text style={{ color: Colors.white }}>Sign In with Google</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setUserData({
+                    name: 'Rahul Sanap',
+                    picture: 'https://cdn3d.iconscout.com/3d/premium/thumb/male-customer-call-service-portrait-6760890-5600697.png?f=webp',
+                    email: 'rahul@gmail.com',
+                    id: 1
+                })}>
+                    <Text>Skip</Text>
+                </TouchableOpacity>
+
+            </View>
+        </View>
+
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 40,
+        marginTop: -24,
+        backgroundColor: '#fff',
+        borderTopRightRadius: 30,
+        borderTopLeftRadius: 30
+    },
+    welcomeText: {
+        fontSize: 35,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    button: {
+        backgroundColor: Colors.primary,
+        padding: 10,
+        margin: 30,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10
+    }
+})
